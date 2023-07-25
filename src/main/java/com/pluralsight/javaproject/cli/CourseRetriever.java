7efca7e270d551a145b8d18b8c9a38e2bcd82b8e
Prisha <pkandhari_be22@thapar.edu>
2023-07-25 13:12:46 +0530

    updating the project

--- src/main/java/com/pluralsight/javaproject/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/javaproject/cli/CourseRetriever.java
@@ -1,22 +1,39 @@
 package com.pluralsight.javaproject.cli;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+//import java.util.logging.Logger;
+
 public class CourseRetriever {
+    private static final Logger LOG=  LoggerFactory.getLogger(CourseRetriever.class);
 
    public static void main( String args[]){
        System.out.println("providing the course info over here");
+       LOG.info("CourseRetriever started using teh logging interface");
        if (args.length==0){
            System.out.println("Please provide an author name as first argument");
+           LOG.warn("need to provide an author name using the logging interface here");
            return;
        }
        try{
            retrieveCourses(args[0]);
        } catch(Exception e){
-           System.out.println("an unexpected error occurs over here");
-           e.printStackTrace();
+//           System.out.println("an unexpected error occurs over here");
+           LOG.error("unexcpected error occurs",e);
+//           e.printStackTrace();
        }
    }
 
     private static void retrieveCourses(String authorId) {
        System.out.println("Retrieving courses for author "+ authorId);
+       LOG.info("retrieving courses using the logging interface for the author "+ authorId);
     }
+
+
+    //after introducing spf4j-api dependancy
+    //setting up a logger
+
+
+
 }
