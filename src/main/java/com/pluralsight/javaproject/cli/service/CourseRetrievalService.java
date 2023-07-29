ec1ea87270d1d780996457c1eeb9f2225cd86148
Prisha <pkandhari_be22@thapar.edu>
2023-07-29 18:14:04 +0530

    using Httpclient web API

--- src/main/java/com/pluralsight/javaproject/cli/service/CourseRetrievalService.java
+++ src/main/java/com/pluralsight/javaproject/cli/service/CourseRetrievalService.java
@@ -7,6 +7,25 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 
+
+// about the http web client API
+//Http Client[started on day3] :
+// An HttpClient can be used to send requests and retrieve their responses.
+// An HttpClient is created through a builder.
+// The newBuilder method returns a builder that creates instances of the default HttpClient implementation.
+// A BodyHandler must be supplied for each HttpRequest sent.
+// The BodyHandler determines how to handle the response provided.
+// -------->send(HttpRequest, BodyHandler) blocks until the request has been sent and the response has been received.
+// -------->sendAsync(HttpRequest, BodyHandler) sends the request and receives the response asynchronously
+//it requires dependencies slf4j-api and slf4j-simple
+//need to ensure dependencies fasterxml.jackson are not showing an error
+//need to update the ReadME also after this
+//this is the course retrieval service
+//we also use the logging feature in this
+//PS_URI signifies the url for the pluralsight courses API
+//this way we can access the courses directly from there
+
+
 public class CourseRetrievalService {
    //for the http web client API
    //pass in authorid to get courses by that author
