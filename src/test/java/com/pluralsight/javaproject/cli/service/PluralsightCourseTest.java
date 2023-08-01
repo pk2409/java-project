4addf5a90a11977c34cd8998574485997d000c76
Prisha <pkandhari_be22@thapar.edu>
2023-08-01 16:58:30 +0530

    json to list type

--- src/test/java/com/pluralsight/javaproject/cli/service/PluralsightCourseTest.java
+++ src/test/java/com/pluralsight/javaproject/cli/service/PluralsightCourseTest.java
@@ -1,29 +1,38 @@
 package com.pluralsight.javaproject.cli.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PluralsightCourseTest {
 
-    @Test
-    void durationInMinutes() {
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            01:08:54.6789, 68
+            00:05:37, 5
+            00:00:00.0, 0
+            """)
+    //it will convert 68,5 and0 to long type for the expected value that we pass
+    //into our method
+    void durationInMinutes(String input,long expected) {
         PluralsightCourse course =
-                new PluralsightCourse("id","Test Course","00:05:37","url",false);
-        assertEquals(5,course.durationInMinutes());
+                new PluralsightCourse("id","Test Course",input,"url",false);
+        assertEquals(expected,course.durationInMinutes());
     }
 
-    @Test
-    void durationInMinutesOverHour() {
-        PluralsightCourse course =
-                new PluralsightCourse("id","Test Course","01:05:37","url",false);
-        assertEquals(65,course.durationInMinutes());
-    }
-
-    @Test
-    void durationInMinutesZero() {
-        PluralsightCourse course =
-                new PluralsightCourse("id","Test Course","00:00:00","url",false);
-        assertEquals(0,course.durationInMinutes());
-    }
+//    @Test
+//    void durationInMinutesOverHour() {
+//        PluralsightCourse course =
+//                new PluralsightCourse("id","Test Course","01:05:37","url",false);
+//        assertEquals(65,course.durationInMinutes());
+//    }
+//
+//    @Test
+//    void durationInMinutesZero() {
+//        PluralsightCourse course =
+//                new PluralsightCourse("id","Test Course","00:00:00","url",false);
+//        assertEquals(0,course.durationInMinutes());
+//    }
 }
