3b08267dafe9648f534e0a5fb70394e61980824c
Prisha <pkandhari_be22@thapar.edu>
2023-08-05 14:05:55 +0530

    adding SQL queries using PreparedStatement

--- course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
+++ course-info-repository/src/main/java/com/pluralsight/courseinfo/repository/CourseJdbcRepository.java
@@ -1,10 +1,23 @@
 package com.pluralsight.courseinfo.repository;
 
+import com.pluralsight.courseinfo.domain.Course;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class CourseJdbcRepository implements CourseRepository{
 
     private static final String H2_DATABASE_URL=
             "jdbc:h2:file:%s;AUTO_SERVER=TRUE;INIT=RUNSCRIPT FROM './db_init.sql'";
 
+
+    private static final String INSERT_COURSE= """
+            MERGE INTO Courses(id,name,length,url)
+                        VALUES(?,?,?,?)
+                        """;
+    //question marks acts as placeholders in a PreparedStatement
+    //use preparedstatement and the API to pass SQL statements
     private final DataSource dataSource;
 
     public CourseJdbcRepository(String databaseFile){
@@ -14,6 +27,9 @@ public class CourseJdbcRepository implements CourseRepository{
     }
     @Override
     public void saveCourse(Course course){
+        Connection connection= dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(INSERT_COURSE);
+
 
     }
 
