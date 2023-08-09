16a974bbf51b07ee10f9dae1c1a0307ff470b071
Prisha <pkandhari_be22@thapar.edu>
2023-08-09 14:31:15 +0530

    working on the CourseStorageServiceTest

--- course-info-cli/src/main/java/com/pluralsight/javaproject/cli/service/PluralsightCourse.java
+++ course-info-cli/src/main/java/com/pluralsight/javaproject/cli/service/PluralsightCourse.java
@@ -25,3 +25,18 @@ public record PluralsightCourse(String id, String title,String duration,String c
 //        LocalTime.parse(duration());
 
 }
+
+//the repository pattern allows us to hide the implementation details
+//so that consumers of the repo API can write clean code
+//the API of the repository can be expressed completely in terms of the domain objects
+//can also change the implemnetation without affecting consumers of repo API
+//intro of API makes the system more testable
+//JDBC: for relational database connectivity API
+//the H2 database provides a JDBC driver as do almost all databases
+//JDBC is used for interaction with databases
+//hibernate library layers on top of the JDBC
+//it has features like object relational mapping out of teh box
+//working on the JdbcDataSource importation
+//updating the PluralsightCourse.java file
+//------->MODULE 4<----------
+//will check for errors caused by this
