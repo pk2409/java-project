2dd9564ac5e9b454bb00b98dab136390c790cd59
Prisha <pkandhari_be22@thapar.edu>
2023-08-11 20:20:55 +0530

    introduced java.ws.rs-api dependency

---
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -0,0 +1,28 @@
+package com.pluralsight.courseinfo.server;
+
+import com.pluralsight.courseinfo.domain.Course;
+import com.pluralsight.courseinfo.repository.CourseRepository;
+import jakarta.ws.rs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/courses")
+public class CourseResource {
+    private static final Logger LOG= LoggerFactory.getLogger(CourseResource.class);
+
+   private final CourseRepository courseRepository;
+
+   public CourseResource(CourseRepository courseRepository){
+       this.courseRepository=courseRepository;
+   }
+
+   public String getCourses(){
+       return courseRepository
+               .getAllCourses()
+               .stream()
+               .map(Course::toString)
+               .collect(Collectors.joining(", "));
+   }
+
+
+}
