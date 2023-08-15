138bce2dab648b910696fbae3975a209934dbb57
Prisha <pkandhari_be22@thapar.edu>
2023-08-15 19:31:52 +0530

    adding notes storing function

--- course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
+++ course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseResource.java
@@ -4,6 +4,7 @@ import com.pluralsight.courseinfo.domain.Course;
 import com.pluralsight.courseinfo.repository.CourseRepository;
 import com.pluralsight.courseinfo.repository.RepositoryException;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -30,16 +31,35 @@ public class CourseResource {
    @GET
    @Produces(MediaType.APPLICATION_JSON)
    public List<Course> getCourses(){
-       return courseRepository
-               .getAllCourses()
-               .stream()
-               .sorted(Comparator.comparing(Course::id))
-               .toList();
+       try {
+           return courseRepository
+                   .getAllCourses()
+                   .stream()
+                   .sorted(Comparator.comparing(Course::id))
+                   .toList();
+       }catch(RepositoryException e){
+           LOG.error("could not retrieve courses from the database",e);
+           throw new NotFoundException();
+       }
    }
 //have to sort the error
     //repository exception is providing error and not letting
     //the application run
     //it has not been caught or declared
     //error in the CourseResource.java
+
+    //adding notes into the course
+    //we are going to expose an endpoint in the course
+    //and endpoint into a particular course/{id}/notes
+    //where the body of teh POST request contains the notes
+    //through such a request, the notes will be added to the course with teh particular id
+    //we need to extend our course-info-repository implementation
+    //with a method that can add notes to a particular course
+    //through an SQL update statement
+    //also need to update our course domain class so that it can contain the notes
+    //used to update the repository to store course notes
+    //need to solve the error due to which teh  JSON file is not visible
+
+
 
 }
